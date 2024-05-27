f3405ca776ff777b619b1f1d61b4c9f62299e576
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2024-05-27 14:31:40 +0700

    feat: search & sort trait eloquent

---
+++ src/Searchable.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Yogameleniawan\SearchSortEloquent;
+
+use Illuminate\Contracts\Database\Query\Builder;
+
+trait Searchable
+{
+    /**
+     * Local Scope query search semua kolom.
+     *
+     * @param \Illuminate\Contracts\Database\Query\Builder $query
+     * @param string $keyword
+     * @param array $columns
+     * @return void
+     */
+    public function scopeSearch(Builder $query, $keyword, array $columns)
+    {
+        if (!empty($columns)) {
+            $query->where(function ($q) use ($keyword, $columns) {
+                foreach ($columns as $index => $column) {
+                    if ($index == 0) {
+                        $q->where($column, 'LIKE', "%{$keyword}%");
+                    } else {
+                        $q->orWhere($column, 'LIKE', "%{$keyword}%");
+                    }
+                }
+            });
+        }
+
+        return $query;
+    }
+}
---
+++ src/Sortable.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Yogameleniawan\SearchSortEloquent;
+
+use Illuminate\Contracts\Database\Query\Builder;
+use Illuminate\Http\Request;
+
+trait Sortable
+{
+    /**
+     * Local Scope query sort semua kolom.
+     *
+     * @param \Illuminate\Contracts\Database\Query\Builder $query
+     * @param \Illuminate\Http\Request $request
+     * @return void
+     */
+    public function scopeSort(Builder $query, Request $request)
+    {
+        if ($request->has('sort_by') && $request->has('sort_order')) {
+            $query->orderBy($request->input('sort_by'), $request->input('sort_order'));
+        } else {
+            $query->orderBy('updated_at', 'DESC');
+        }
+    }
+}
